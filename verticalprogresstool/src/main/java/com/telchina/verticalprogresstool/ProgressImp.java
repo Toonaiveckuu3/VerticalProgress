81cf06dd4a9be28a56faf316d0e3ccadfff4ba63
ZH <hengzhang126@126.com>
2016-04-14 14:18:00 +0800

    111

--- verticalprogresstool/src/main/java/com/telchina/verticalprogresstool/ProgressImp.java
+++ verticalprogresstool/src/main/java/com/telchina/verticalprogresstool/ProgressImp.java
@@ -8,14 +8,14 @@ import com.telchina.verticalprogresstool.partstepview.PartStepView;
 
 /**
  * 添加垂直进度接口实现
- *
+ * <p>
  * Created by ZH on 2016/4/13.
  */
 public class ProgressImp implements IProgress {
     private Context mContext;
     private PartStepView mPartStep;
 
-    public ProgressImp(Context context){
+    public ProgressImp(Context context) {
         this.mContext = context;
         mPartStep = new PartStepView(mContext);
     }
@@ -23,10 +23,10 @@ public class ProgressImp implements IProgress {
     @Override
     public View addPartStep(View convertView) {
         LinearLayout linearLayout = new LinearLayout(mContext);
-        LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        linearLayout.addView(mPartStep,params);
-        linearLayout.addView(convertView,params);
+        linearLayout.addView(mPartStep, params);
+        linearLayout.addView(convertView, params);
 
         return linearLayout;
     }
